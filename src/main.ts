25ed1a61d9fdbe2a631a15f4c71f1b412e16754f
Samuel Pereira <pereirasamueljp@gmail.com>
2022-06-22 02:04:55 -0300

    products exemple

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
-import express from 'express'
+import express from 'express';
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/products', async (req, appRes) => {
+  let res = await fetch('https://dummyjson.com/products/');
+  if (res.ok) {
+    let data = await res.json();
+    const { products, total, limit } = data
+    products.forEach((product: any) => {
+      product.colors = [
+        `#808080`,
+        `#228b22`,
+        `#f5f5f5`,
+        `#ff7c00`
+      ];
+      product.measureName = "Weight";
+      product.measure = [
+        "Kg",
+        "gm"
+      ];
+    });
+    appRes.send(products);
+    return;
+  }
+  appRes.status(500).send('Error loading products. Please, try again!');
+
 })
 
 app.listen(port, () => {
