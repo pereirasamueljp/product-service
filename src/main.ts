33d2f28811b9b59287693825db4e445187bdbd0e
Samuel Pereira <pereirasamueljp@gmail.com>
2022-06-23 02:10:35 -0300

    Swagger and get products

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import express from 'express';
+import cors from 'cors';
+import swaggerUi from 'swagger-ui-express'
+
+const swaggerJsonConf = require('./swagger.json')
+
 
 const app = express()
 const port = 3000
 
+app.use(cors())
+
+app.use('/api-doc', swaggerUi.serve, swaggerUi.setup(swaggerJsonConf))
 app.get('/products', async (req, appRes) => {
-  let res = await fetch('https://dummyjson.com/products/');
+
+  let url = urlQueryBuilder(req.query);
+
+  let res = await fetch(url);
   if (res.ok) {
     let data = await res.json();
-    const { products, total, limit } = data
+
+    const { products, total, limit} = data;
     products.forEach((product: any) => {
+
       product.colors = [
         `#808080`,
         `#228b22`,
@@ -21,12 +34,29 @@ app.get('/products', async (req, appRes) => {
         "gm"
       ];
     });
-    appRes.send(products);
+    appRes.send({ products, total, limit });
     return;
   }
   appRes.status(500).send('Error loading products. Please, try again!');
 
 })
+
+function urlQueryBuilder(query: any) {
+  const { skip, limit, sortByPrice, sortByAlphabet, sortByLowToHigh, category } = query;
+  let mainUrl = 'https://dummyjson.com/products';
+
+  category ? mainUrl += `/category/${category}` : null;
+  if (limit) {
+    mainUrl += `?limit=${limit}`;
+    skip ? mainUrl += `&skip=${skip}` : null;
+    return mainUrl;
+  }
+  if (skip) {
+    mainUrl += `?skip=${skip}`;
+    return mainUrl;
+  }
+  return mainUrl
+}
 
 app.listen(port, () => {
   console.log(`Server runing on port ${port}`)
